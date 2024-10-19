81a392a8b4c79189f5164b2f167867414ce33de4
Baguma Martin <amanyamartin05@gmail.com>
2024-10-19 16:08:50 +0200

    Initial Commit

---
+++ Trekrr/TrekrrApp.swift
@@ -0,0 +1,32 @@
+//
+//  TrekrrApp.swift
+//  Trekrr
+//
+//  Created by Martin on 2024. 10. 19..
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TrekrrApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TrekrrTests/TrekrrTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrrTests.swift
+//  TrekrrTests
+//
+//  Created by Martin on 2024. 10. 19..
+//
+
+import Testing
+@testable import Trekrr
+
+struct TrekrrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
